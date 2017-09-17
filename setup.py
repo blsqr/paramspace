0acdc301c5947d67933743b3220b922926bd0365
Yunus Sevinchan <YunusSevinchan@gmail.com>
2017-09-17 19:07:26 +0200

    Reverted version id to 0.9; added LICENCE and classifiers; renamed README

--- setup.py
+++ setup.py
@@ -3,11 +3,17 @@
 from distutils.core import setup
 
 setup(name='ParamSpace',
-      version='1.0',
-      description='Iterators over a multidimensional dictionary.',
+      version='0.9',
+      description='Multidimensional parameter space with dictionaries at each point.',
       long_description='Classes that allow easy iteration over a multidimensional parameter space, generating dictionaries at each point in this parameter space.',
       author='Yunus Sevinchan',
       author_email='YunusSevinchan@gmail.com',
-      # url='',
       packages=['pspace'],
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.5',
+          'Topic :: Utilities'
+          ],
      )
