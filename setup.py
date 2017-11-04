bc522be55f1e0a69faae07729f40f8d8220594f2
Yunus Sevinchan <YunusSevinchan@gmail.com>
2017-11-04 10:55:40 +0100

    Changed package name, updaed package setup, in preparation of v0.9.1

--- setup.py
+++ setup.py
@@ -3,18 +3,23 @@
 from setuptools import setup
 
 setup(name='ParamSpace',
-      version='0.9',
+      version='0.9.1',
       description='Multidimensional parameter space with dictionaries at each point.',
       long_description='Classes that allow easy iteration over a multidimensional parameter space, generating dictionaries at each point in this parameter space.',
       author='Yunus Sevinchan',
       author_email='YunusSevinchan@gmail.com',
       url='https://github.com/blusquare/pspace',
-      packages=['pspace'],
+      licence='MIT',
       classifiers=[
           'Development Status :: 5 - Production/Stable',
           'Intended Audience :: Science/Research',
           'License :: OSI Approved :: MIT License',
           'Programming Language :: Python :: 3.5',
           'Topic :: Utilities'
+          ],
+      packages=['paramspace'],
+      include_package_data=True,
+      install_requires=[
+          'numpy>=1.13',
           ],
      )
