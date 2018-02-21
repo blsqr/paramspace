5a9b2db14e1ae555af205b730ab82ca28e1ff7ea
Yunus Sevinchan <Yunus.Sevinchan@iup.uni-heidelberg.de>
2018-02-21 20:26:12 +0100

    Re-implement base functionality of ParamDim
    Very pythonistic, much wow. Tests still partly failing though

--- paramspace/paramdim.py
+++ paramspace/paramdim.py
@@ -2,7 +2,8 @@
 
 import copy
 import logging
-from typing import Iterable
+import warnings
+from typing import Iterable, Union
 
 import numpy as np
 
@@ -14,26 +15,59 @@ log = logging.getLogger(__name__)
 class ParamDimBase:
     """The ParamDim base class."""
 
-    def __init__(self, *, values: Iterable=None, enabled: bool=True, **kwargs) -> None:
+    def __init__(self, *, default, values: Iterable=None, enabled: bool=True, order: float=np.inf, name: str=None, **kwargs) -> None:
         """Initialise the ParamDim.
         
         Args:
-            values (Iterable, optional): Which discrete values this parameter dimension can take. This argument takes precedence over any constructors given in the kwargs (like range, linspace, …).
-            enabled (bool, optional): Whether this values is enabled and should be used in a sweep. Default: True
-            **kwargs: Description
-        """
+            default: default value of this parameter dimension
+            values (Iterable, optional): Which discrete values this parameter
+                dimension can take. This argument takes precedence over any
+                constructors given in the kwargs (like range, linspace, …).
+            enabled (bool, optional): Whether this parameter dimension is
+                enabled and should be used in a sweep. Default: True
+            order (float, optional): # TODO
+            name (str, optional): # TODO
+            **kwargs: Constructors for the `values` argument, valid keys are
+                `range`, `linspace`, and `logspace`; corresponding values are
+                expected to be iterables and are passed to `range(*args)`,
+                `np.linspace(*args)`, or `np.logspace(*args)`, respectively.
         
-        # Initialize attributes that are managed by properties
-        self._vals = None
-        self._name = None
-        self._order = np.inf
+        Raises:
+            ValueError: Description
+        """
+        # Initialize attributes that are not managed
+        self.name      = name
+        self.order     = order
+        self.enabled   = enabled
+        
+        # Initialize attributes that are managed by properties or methods
+        self._vals     = None
+        self._state    = None
 
         # Carry over arguments
-        self.enabled = enabled
+        self.default   = default
+        self.values    = values
 
+        # If applicable, parse other keyword arguments
+        if (self.values is None
+            and any([k in kwargs for k in ('range', 'linspace', 'logspace')])):
+            # Need to parse the additional keyword arguments to generate the values attribute from it
+            if len(kwargs) > 1:
+                warnings.warn("{}.__init__ was called with multiple additional `**kwargs`; only one of these will be used! The order is: `range`, `linspace`, `logspace`.".format(self.__class__.__name__))
 
+            if 'range' in kwargs:
+                self.values = range(*kwargs.get('range'))
+            elif 'linspace' in kwargs:
+                self.values = np.linspace(*kwargs.get('linspace'))
+            elif 'logspace' in kwargs:
+                self.values = np.logspace(*kwargs.get('logspace'))
+            # else: not possible
 
-        # Parse other keyword arguments
+        elif kwargs and self.values is not None:
+            warnings.warn("{}.__init__ was called with both the argument `values` and additional `**kwargs`: {}. With `values` present, the additional keyword arguments are ignored.".format(self.__class__.__name__, kwargs), warnings.UserWarning)
+
+        else:
+            raise ValueError("No argument `values` or other `**kwargs` specified, but at least one of these is needed for initialisation.")
 
 
     # Properties ..............................................................
@@ -54,15 +88,73 @@ class ParamDimBase:
             values (Iterable): Description
         
         Raises:
-            ValueError: Raised when a span value was already set
+            RuntimeError: Raised when a span value was already set before
+            ValueError: Raised when the given iterable was not at least of length 1
         """
         if self._vals is None:
+            if not len(values):
+                raise ValueError("Argument `values` needs to be an iterable of at least length 1, was " + str(values))
+
             self._vals = values
+
         else:
-            raise ValueError("Span is already set and cannot be set again.")
+            raise RuntimeError("Span is already set and cannot be set again.")
+
+    @property
+    def state(self):
+        """The current iterator state"""
+        return self._state
+
+    @property
+    def current_value(self):
+        """If in an iteration: return the value according to the current state. If not in an iteration and/or disabled, return the default value."""
+        if self.enabled and self.state is not None:
+            return self.values[self.state]
+        else:
+            return self.default
+
+    # Magic methods ...........................................................
+    # TODO str, repr, len
+
+    def __len__(self):
+        """ """
+        if not self.enabled:
+            return 1
+        else:
+            return len(self.values)
+
+    # Iterator functionality ..................................................
+
+    def __next__(self):
+        """Move to the next valid state and return the corresponding parameter value."""
+
+        if not self.enabled:
+            log.debug("__next__ called on disabled ParamDim")
+            raise StopIteration
+
+        log.debug("__next__ called")
+
+        if self.state is None:
+            # Start of iteration: Set state and return first value
+            self._state = 0
+            return self.current_value
+
+        else:
+            # Continuation of iteration: increment state and 
+            self._state += 1
+
+            if self.state == len(self):
+                # Reached the end of the iteration: reset state
+                # NOTE: unlike with other Python iterators, the object is still usable after this.
+                self._state = None
+                raise StopIteration
+            else:
+                return self.current_value
 
 
     # Public API ..............................................................
+
+
 
 
     # Non-public API ..........................................................
--- tests/test_paramdim.py
+++ tests/test_paramdim.py
@@ -10,14 +10,14 @@ def various_pdims(request):
     """Used to setup various pspan objects to be tested on."""
     pds = {}
 
-    pds['span1']     = ParamDim(values=[1,2,3], default=0)
-    pds['span2']     = ParamDim(values=[1., 2, 'three', (0, 1, 0, 0)])
-    pds['range']     = ParamDim(range=[1, 4, 1])
-    pds['linspace']  = ParamDim(linspace=[1, 3, 3, True])
-    pds['logspace']  = ParamDim(logspace=[-1, 1, 11])
-    pds['named']     = ParamDim(values=[1,2,3], name="named_span")
-    pds['with_order']= ParamDim(values=[1,2,3], order=42)
-    pds['disabled']  = ParamDim(values=[1,2,3], enabled=False)
+    pds['span1']     = ParamDim(default=0, values=[1,2,3])
+    pds['span2']     = ParamDim(default=0, values=[1., 2, 'three', (1,0,0)])
+    pds['range']     = ParamDim(default=0, range=[1, 4, 1])
+    pds['linspace']  = ParamDim(default=0, linspace=[1, 3, 3, True])
+    pds['logspace']  = ParamDim(default=0, logspace=[-1, 1, 11])
+    pds['named']     = ParamDim(default=0, values=[1,2,3], name="named_span")
+    pds['with_order']= ParamDim(default=0, values=[1,2,3], order=42)
+    # pds['disabled']  = ParamDim(values=[1,2,3], enabled=False)
 
     return pds
 
@@ -35,9 +35,26 @@ def test_properties(various_pdims):
     """Test all properties and whether they are write-protected."""
     pass
 
-def test_iteration(various_pdims):
+def test_iteration():
     """Tests whether the iteration over the span's state works."""
-    pass
+    pd = ParamDim(values=[0,1,2])
+
+    # First iteration
+    assert pd.__next__() == 0
+    assert pd.__next__() == 1
+    assert pd.__next__() == 2
+    with pytest.raises(StopIteration):
+        pd.__next__()
+
+    # Should be able to iterate again
+    assert pd.__next__() == 0
+    assert pd.__next__() == 1
+    assert pd.__next__() == 2
+    with pytest.raises(StopIteration):
+        pd.__next__()
+
+def test_disabled_without_default():
+    """Tests whether an exception is raised when disabling is tried without a default value being present."""
 
 
 # Tests still to write --------------------------------------------------------
