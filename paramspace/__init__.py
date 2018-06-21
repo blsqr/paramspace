96d81fe97087bc919ad17aabe28f159d98e755ff
Yunus Sevinchan <Yunus.Sevinchan@iup.uni-heidelberg.de>
2018-06-21 14:40:48 +0200

    Bump version to 1.0
    Yay. :)

--- paramspace/__init__.py
+++ paramspace/__init__.py
@@ -1,4 +1,12 @@
-__version__ = "1.0b"
+"""The paramspace package provides classes to conveniently create parameter
+spaces and iterate over them.
+
+To that end, any dict-like object can be filled with `ParamDim` objects to
+create parameter dimensions. When passing this dict-like object to
+`ParamSpace`, it is possible to iterate over the points in parameter space ...
+"""
+
+__version__ = "1.0"  # NOTE also change the value in setup.py!
 
 from paramspace.paramspace import ParamSpace
 from paramspace.paramdim import ParamDim, CoupledParamDim
