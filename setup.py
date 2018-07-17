65b6cefffabef6ebd41e18df74d6d3286d9424cc
Yunus Sevinchan <Yunus.Sevinchan@iup.uni-heidelberg.de>
2018-07-17 14:42:43 +0200

    Drop test job for python 3.5 altogether

--- setup.py
+++ setup.py
@@ -19,7 +19,6 @@ setup(name='ParamSpace',
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Topic :: Utilities'
