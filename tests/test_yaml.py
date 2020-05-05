133ecff88f59c77b7ce37a66bd60b2b06d9daa84
Yunus Sevinchan <yunussevinchan@gmail.com>
2020-05-05 11:58:26 +0200

    Improve test YAML strings formatting

--- tests/test_yaml.py
+++ tests/test_yaml.py
@@ -12,137 +12,121 @@ from paramspace.yaml import *
 
 @pytest.fixture()
 def yamlstrs() -> dict:
-    """Prepares a list of yaml strings to test agains"""
-    strs = {}
-
-    strs[
-        "pspace_only"
-    ] = """
-mapping: !pspace
-  a: 1
-  b: 2
-  c: 3
-mapping_sorted: !pspace
-  a: 1
-  c: 3
-  b: 2
-  foo:
-    bar: 1
-    baz: 2
-mapping_unsorted: !pspace-unsorted
-  a: 1
-  c: 3
-  b: 2
-  foo:
-    bar: 1
-    baz: 2
-    """
-
-    strs[
-        "pdims_only"
-    ] = """
-pdims:
- - !pdim
-   default: 0
-   values: [1,2,3]
- - !pdim
-   default: 0
-   range: [10]
- - !pdim
-   default: 0
-   linspace: [1,2,3]
- - !pdim
-   default: 0
-   logspace: [1,2,3]
- - !pdim-default
-   default: 0
-   values: [1,2,3]
-    """
-
-    strs[
-        "cpdims_only"
-    ] = """
-pdims:
- - !coupled-pdim
-   target_name: [foo, bar]
- - !coupled-pdim
-   target_name: [foo, bar]
- - !coupled-pdim
-   target_name: [foo, bar]
- - !coupled-pdim
-   target_name: [foo, bar]
- - !coupled-pdim-default
-   target_name: [foo, bar]
-   default: 0
-    """
-
-    strs[
-        "slice"
-    ] = """
-slices:
- - !slice 5
- - !slice [5]
- - !slice [0, ~]
- - !slice [~, 0]
- - !slice [0, 10, 2]
- - !slice [0, 10, None]
- - !slice [2, None, 2]
-    """
-
-    strs[
-        "range"
-    ] = """
-ranges:
- - !range 10
- - !range [10]
- - !range [5, 10]
- - !range [5, 10, 2]
-    """
-
-    strs[
-        "listgen"
-    ] = """
-lists:
- - !listgen [10]
- - !listgen [0, 10, 2]
- - !listgen
-   from_range: [0, 10, 3]
-   unique: true
-   append: [100]
-   remove: [0]
-   sort: true
- - !listgen [5, 10, 2]
-    """
-
-    # Failing or warning cases
-    strs[("_pspace_scalar", TypeError)] = "scalar_node: !pspace 1"
-
-    strs[("_pdim1", TypeError)] = "not_a_mapping: !pdim 1"
-    strs[("_pdim2", TypeError)] = "not_a_mapping: !pdim [1,2,3]"
-    strs[("_pdim3", TypeError)] = "wrong_args: !pdim {foo: bar}"
-
-    strs[("cpdim1", TypeError)] = "not_a_mapping: !coupled-pdim 1"
-    strs[("cpdim2", TypeError)] = "not_a_mapping: !coupled-pdim [1,2,3]"
-    strs[("cpdim3", TypeError)] = "wrong_args: !coupled-pdim {foo: bar}"
-
-    strs[
-        ("cpdim4", None, DeprecationWarning)
-    ] = """
-too_many_args: !coupled-pdim
-  target_name: [foo, bar]
-  default: 0
-  use_coupled_default: True
-    """
-    strs[
-        ("cpdim5", None, DeprecationWarning)
-    ] = """
-too_many_args: !coupled-pdim
-  target_name: [foo, bar]
-  values: [1,2,3]
-  use_coupled_values: True
-    """
-
-    strs[("_listgen_scalar", TypeError)] = "scalar_node: !listgen foo"
+    """Prepares a list of yaml strings to test against"""
+    # NOTE Leading indentation is ignored by yaml
+    strs = {
+        "pspace_only": """
+            mapping: !pspace
+              a: 1
+              b: 2
+              c: 3
+            mapping_sorted: !pspace
+              a: 1
+              c: 3
+              b: 2
+              foo:
+                bar: 1
+                baz: 2
+            mapping_unsorted: !pspace-unsorted
+              a: 1
+              c: 3
+              b: 2
+              foo:
+                bar: 1
+                baz: 2
+        """,
+        "pdims_only": """
+            pdims:
+             - !pdim
+               default: 0
+               values: [1,2,3]
+             - !pdim
+               default: 0
+               range: [10]
+             - !pdim
+               default: 0
+               linspace: [1,2,3]
+             - !pdim
+               default: 0
+               logspace: [1,2,3]
+             - !pdim-default
+               default: 0
+               values: [1,2,3]
+        """,
+        "cpdims_only": """
+            pdims:
+             - !coupled-pdim
+               target_name: [foo, bar]
+             - !coupled-pdim
+               target_name: [foo, bar]
+             - !coupled-pdim
+               target_name: [foo, bar]
+             - !coupled-pdim
+               target_name: [foo, bar]
+             - !coupled-pdim-default
+               target_name: [foo, bar]
+               default: 0
+        """,
+        "slice": """
+            slices:
+             - !slice 5
+             - !slice [5]
+             - !slice [0, ~]
+             - !slice [~, 0]
+             - !slice [0, 10, 2]
+             - !slice [0, 10, None]
+             - !slice [2, None, 2]
+        """,
+        "range": """
+            ranges:
+             - !range 10
+             - !range [10]
+             - !range [5, 10]
+             - !range [5, 10, 2]
+        """,
+        "listgen": """
+            lists:
+             - !listgen [10]
+             - !listgen [0, 10, 2]
+             - !listgen
+               from_range: [0, 10, 3]
+               unique: true
+               append: [100]
+               remove: [0]
+               sort: true
+             - !listgen [5, 10, 2]
+        """,
+        #
+        # Failing or warning cases
+        ("_pspace_scalar", TypeError): "scalar_node: !pspace 1",
+        ("_pdim1", TypeError): "not_a_mapping: !pdim 1",
+        ("_pdim2", TypeError): "not_a_mapping: !pdim [1,2,3]",
+        ("_pdim3", TypeError): "wrong_args: !pdim {foo: bar}",
+        ("cpdim1", TypeError): "not_a_mapping: !coupled-pdim 1",
+        ("cpdim2", TypeError): "not_a_mapping: !coupled-pdim [1,2,3]",
+        ("cpdim3", TypeError): "wrong_args: !coupled-pdim {foo: bar}",
+        (
+            "cpdim4",
+            None,
+            DeprecationWarning,
+        ): """
+            too_many_args: !coupled-pdim
+              target_name: [foo, bar]
+              default: 0
+              use_coupled_default: True
+        """,
+        (
+            "cpdim5",
+            None,
+            DeprecationWarning,
+        ): """
+            too_many_args: !coupled-pdim
+              target_name: [foo, bar]
+              values: [1,2,3]
+              use_coupled_values: True
+        """,
+        ("_listgen_scalar", TypeError): "scalar_node: !listgen foo",
+    }
 
     return strs
 
