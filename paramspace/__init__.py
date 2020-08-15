64efba92b0149bd4e896a0f28b1baba26c489444
Yunus Sevinchan <yunussevinchan@gmail.com>
2020-08-15 15:42:35 +0200

    Add changelog entry and bump version

--- paramspace/__init__.py
+++ paramspace/__init__.py
@@ -12,7 +12,7 @@ define the parameter space fully from YAML configuration files, using custom
 YAML tags.
 """
 
-__version__ = "2.5.2"
+__version__ = "2.5.3"
 
 from .paramdim import CoupledParamDim, ParamDim
 from .paramspace import ParamSpace
