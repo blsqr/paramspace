bf7b89bdef91db58ce97fca3b9fd9d09306d9ddb
Yunus Sevinchan <yunussevinchan@gmail.com>
2025-11-07 11:53:08 +0100

    Activate Python 3.14 test job

--- setup.py
+++ setup.py
@@ -143,7 +143,7 @@ setup(
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.13",
-        # "Programming Language :: Python :: 3.14",  # TODO ... once working
+        "Programming Language :: Python :: 3.14",
         #
         "Topic :: Utilities",
         "Typing :: Typed",
